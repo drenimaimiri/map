a921c38f9a4f778f63621905d1e16bbffa21cbb4
drenimaimiri <drendedincajcoders@gmail.com>
2025-05-05 13:57:43 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+var map = L.map('map').setView([42.663343251481116, 21.171257881212398], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+var greenIcon = L.icon({
+    iconUrl: 'leaf-green.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
+L.marker([51.5, -0.09], {icon: greenIcon}).addTo(map);
